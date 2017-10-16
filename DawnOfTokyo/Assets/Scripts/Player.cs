8625a3eb6e84ea19798de328ca092e66d1b4a725
Unai <unai.santos@live.u-tad.com>
2017-10-16 12:39:27 +0200

    Update Player.cs

--- DawnOfTokyo/Assets/Scripts/Player.cs
+++ DawnOfTokyo/Assets/Scripts/Player.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	
+	Rigidbody body;
+	public float vel;
+	
 	// Use this for initialization
 	void Start () {
-		
+	body = GetComponent<Rigidbody>();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    void FixedUpdate () {
 		
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (h != 0 || v != 0)
+		{
+			body.velocity = new Vector3 (vel * h, 0, vel * v);
+		}
+
+
 	}
 }
