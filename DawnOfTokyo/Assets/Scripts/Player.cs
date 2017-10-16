80c6aad7f3fc34aa76370fe70eb344fe5b1f86f9
Unai <unai.santos@live.u-tad.com>
2017-10-16 13:22:35 +0200

    Update Player.cs

--- DawnOfTokyo/Assets/Scripts/Player.cs
+++ DawnOfTokyo/Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	
+
 	Rigidbody body;
 	public float vel;
-	
-	// Use this for initialization
-	void Start () {
-	body = GetComponent<Rigidbody>();
 
-	}
+
+	private bool _jumped = false;
+   	public float jumpForce = 3f;
+	float _distToGround;
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+        _distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
     void FixedUpdate () {
 		
 		float h = Input.GetAxis ("Horizontal");
@@ -23,6 +31,22 @@ public class Player : MonoBehaviour {
 			body.velocity = new Vector3 (vel * h, 0, vel * v);
 		}
 
-
+		  if (isGrounded()) {
+             if (Input.GetButton("Jump")) {
+                 jump();
+             }
+         }
 	}
+	void jump() {
+         Debug.Log("jumping");
+         if (!_jumped) {
+             _jumped = true;
+             Vector3 force = transform.up * jumpForce;
+             body.AddRelativeForce(force, ForceMode.Impulse);
+         }
+         _jumped = false;
+     }
+	     bool isGrounded() {
+         return Physics.Raycast(transform.position, -Vector3.up, _distToGround + .1F);
+     }
 }
